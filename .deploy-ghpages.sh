555ef392de279b5bbcedc4d610906a8891d71238
Njagi Mwaniki <njagi@urbanslug.com>
2017-09-13 17:42:01 +0300

    Use global js doc in the yarn script section
    Signed-off-by: Njagi Mwaniki <njagi@urbanslug.com>

--- .deploy-ghpages.sh
+++ .deploy-ghpages.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-echo "Preparing to push docs to github"
-( cd out/
- git init
- git config user.name "Njagi Mwaniki"
- git config user.email "njagi@urbanslug.com"
- git add .
- git commit -m "Deployed to Github Pages"
- git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
-)
+echo "Generating an pushing docs to github pages"
+cd docs/html/
+git init
+git config user.name "Travis CI"
+git add .
+git commit -m "API docs."
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
