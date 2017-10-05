1fdf95ec9bda5f310fcd1c96e6a5078ba0bde3a5
Njagi Mwaniki <njagi@urbanslug.com>
2017-10-05 23:46:20 +0300

    Move Messenger profile API code into borq
    Signed-off-by: Njagi Mwaniki <njagi@urbanslug.com>

--- index.js
+++ index.js
@@ -1,1 +1,71 @@
-require('./examples/survey.js');
+const borq = require('borq');
+const facebookBot = borq.facebookBot;
+
+/*
+* Set Messenger Profile API
+* https://developers.facebook.com/docs/messenger-platform/reference/messenger-profile-api
+*/
+facebookBot.setGetStarted('start');
+facebookBot.setGreeting('Hello, I am a bot.');
+facebookBot.setMenu([
+  {
+    locale: 'in_ID',
+    composer_input_disabled: true,
+    call_to_actions: [
+      {
+        type: 'web_url',
+        title: 'FAQ',
+        url: 'https://goodbotai.github.io/borq/',
+        webview_height_ratio: 'full',
+      }
+    ]
+  }, {
+    locale: 'default',
+    composer_input_disabled: true,
+    call_to_actions: [
+      {title: '',
+       type: 'nested',
+       call_to_actions: [
+         {
+           title: 'English',
+           type: 'postback',
+           payload: 'english',
+         },
+        {
+          title: 'Bahasa',
+          type: 'postback',
+          payload: 'bahasa',
+        },
+       ],
+      },
+    ]
+  }
+]);
+
+facebookBot.on('facebook_postback', (bot, message) => {
+  if (message.payload === 'start') {
+    bot.startConversation(message, (err, convo) => {
+      convo.addMessage('Started');
+    });
+  } else if (message.payload === 'english') {
+    bot.startConversation(message, (err, convo) => {
+      convo.addMessage('Hello');
+    });
+  } else if (message.payload === 'bahasa') {
+    bot.startConversation(message, (err, convo) => {
+      convo.addMessage('Halo');
+    });
+  }
+});
+
+facebookBot.hears(['hello'],
+                  'message_received',
+                  (bot, message) => {
+                    bot.reply(message, 'How\'s your day going?');
+});
+
+facebookBot.hears([/([a-z])\w+/gi],
+                  'message_received',
+                  function(bot, message) {
+                    bot.reply(message, 'I don\'t know that word yet');
+});
