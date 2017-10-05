c3648fc98a1e5297feb16eda8191b5d9cc45027c
Njagi Mwaniki <njagi@urbanslug.com>
2017-10-05 22:34:45 +0300

    Style the tables so they are more legible
    Signed-off-by: Njagi Mwaniki <njagi@urbanslug.com>

--- .deploy-ghpages.sh
+++ .deploy-ghpages.sh
@@ -2,7 +2,8 @@
 
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
     echo "Generating and pushing docs to github pages"
-    cd  out/ || exit
+    cat docs/_css/table.css >> out/styles/jsdoc-default.css || exit
+    cd out/ || exit
     git init
     git config user.name "Travis CI"
     git add .
