983bd5243ba5fa0c55143a6e6f916124990baa91
Njagi Mwaniki <njagi@urbanslug.com>
2018-02-16 12:50:22 +0300

    Move aggregate tests to Jest
    Signed-off-by: Njagi Mwaniki <njagi@urbanslug.com>

--- __tests__/Aggregate.js
+++ __tests__/Aggregate.js
@@ -25,65 +25,63 @@ const conversation = {
   },
 };
 
-function testAggregate() {
-  const date = new Date(unixTimestamp);
-  const aggregated = aggregate.aggregate(conversation);
-  const onaSubmission = aggregate.genOnaSubmission(aggregated);
-  const instanceID = onaSubmission.submission.meta.instanceID;
+const date = new Date(unixTimestamp);
+const aggregated = aggregate.aggregate(conversation);
+const onaSubmission = aggregate.genOnaSubmission(aggregated);
+const instanceID = onaSubmission.submission.meta.instanceID;
 
-  describe('aggregate', () => {
-    it('returns an object containing a user ID under psid', () => {
-      assert.equal(user, aggregate.aggregate(conversation).psid);
-    });
-
-    it('returns an object containing responses under responses', () => {
-      assert.ok(aggregate.aggregate(conversation).responses);
-    });
-
-    it('the aggregated conversation has ISO 8601 timestamps in it', () => {
-      const res = aggregate.aggregate(conversation).responses;
-      assert.deepEqual(date, res.playWithShopToysTimeStamp.text);
-    });
+describe('aggregate', () => {
+  it('returns an object containing a user ID under psid', () => {
+    expect(aggregate.aggregate(conversation).psid).toBe(user);
   });
 
-  describe('extractTimestamps', () => {
+  it('returns an object containing responses under responses', () => {
+    expect(aggregate.aggregate(conversation).responses);
+  });
+
+  it('the aggregated conversation has ISO 8601 timestamps in it', () => {
+    const res = aggregate.aggregate(conversation).responses;
+    expect(res.playWithShopToysTimeStamp.text).toEqual(date);
+  });
+});
+
+describe('extractTimestamps', () => {
+  const responses = conversation.responses;
+
+  it('converts unix timestamps to ISO 8601 timestamps', () => {
     const responses = conversation.responses;
+    const timestamps = aggregate.extractTimeStamps(responses);
+    expect(timestamps.playWithShopToysTimeStamp.text).toEqual(date);
+  });
 
-    it('converts unix timestamps to ISO 8601 timestamps', () => {
-      const responses = conversation.responses;
-      const timestamps = aggregate.extractTimeStamps(responses);
-      assert.deepEqual(date, timestamps.playWithShopToysTimeStamp.text);
-    });
-
-    it('creates a question<TimeStamp> reponse for responses with a timestamp', () => {
-      const responsesWithTimeStamps = aggregate.extractTimeStamps(responses);
-      const expectedArray = utils.mergeObjects(
+  it('creates a question<TimeStamp> reponse for responses with a timestamp', () => {
+    const responsesWithTimeStamps = aggregate.extractTimeStamps(responses);
+    const expectedArray = Object.values(
+      utils.mergeObjects(
         Object.keys(responses),
         Object.keys(responses).map((e) => e + 'TimeStamp')
-      );
-      assert.deepEqual(expectedArray, Object.keys(responsesWithTimeStamps));
-    });
+      )
+    );
+    expect(Object.keys(responsesWithTimeStamps)).toEqual(expectedArray);
+  });
+});
+
+describe('genOnaSubmission', () => {
+  it('generates a valid submission', () => {
+    const expected = {
+      submission: {
+        playWithShopToys: 'yes',
+        playWithShopToysTimeStamp: date,
+        meta: {instanceID},
+      },
+    };
+    expect(onaSubmission).toEqual(expected);
   });
 
-  describe('genOnaSubmission', () => {
-    it('generates a valid submission', () => {
-      const expected = {
-        submission: {
-          playWithShopToys: 'yes',
-          playWithShopToysTimeStamp: date,
-          meta: {instanceID},
-        },
-      };
-      assert.deepEqual(expected, onaSubmission);
-    });
-
-    it('has an instance id for each submission', () => {
-      assert.ok(instanceID);
-    });
-
-    it('generates repeat groups in general');
-    it('is not a convoluted mess');
+  it('has an instance id for each submission', () => {
+    expect(instanceID);
   });
-}
 
-module.exports = {testAggregate, conversation};
+  it('generates repeat groups in general');
+  it('is not a convoluted mess');
+});
